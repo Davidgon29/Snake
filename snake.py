fb1c484b33cb3d6becd3e9e61210aceaff3e3e0e
AlgoLozano <a01661437@tec.mx>
2022-03-23 10:30:48 -0600

    Resolviendo problema 2, cambio de color en cada inicio de juego

--- snake.py
+++ snake.py
@@ -1,18 +1,23 @@
 from random import randrange
 from turtle import *
 from freegames import square, vector
+import random
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+colores = ["cyan", "blue", "orange", "purple", "yellow", "green", "brown", "gray"] #Lista de colores para escoger
+colorSnake = random.choice(colores) #Selecciona un color aleatorio de la lista para la vibora
+colorFood = random.choice(colores) #Selecciona un color aleatorio de la lista para la comida
+if colorFood == colorSnake:
+    colorSnake = random.choice(colores) #Si el color es el mismo, seleccionar un nuevo color
 
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
-
-
+    
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
@@ -40,9 +45,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, colorSnake)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, colorFood)
     update()
     ontimer(move, 100)
 
@@ -56,7 +61,6 @@ def moveFood():
         food.y = randrange(-15, 15) * 10
 
     ontimer(moveFood, 500)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
